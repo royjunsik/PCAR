36564a265878b6cab5cae49a4190b82e14eae6b6
roy <royjunsik@gmail.com>
2024-08-08 22:58:56 -0900

    m

--- ROY/Multi_Camera_Adapter_V2.2_python/AdapterTestDemo.py
+++ ROY/Multi_Camera_Adapter_V2.2_python/AdapterTestDemo.py
@@ -24,20 +24,7 @@ def main():
     gp.output(11, False)
     gp.output(12, True)
     capture(2)
-    print('Start testing the camera C')
-    i2c = "i2cset -y 1 0x70 0x00 0x06"
-    os.system(i2c)
-    gp.output(7, False)
-    gp.output(11, True)
-    gp.output(12, False)
-    capture(3)
-    print('Start testing the camera D')
-    i2c = "i2cset -y 1 0x70 0x00 0x07"
-    os.system(i2c)
-    gp.output(7, True)
-    gp.output(11, True)
-    gp.output(12, False)
-    capture(4)
+
     
 def capture(cam):
     cmd = "libcamera-still -o capture_%d.jpg" % cam
