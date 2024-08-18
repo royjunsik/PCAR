1053d7ca65be2931f14821e81d0f8a2aa562335e
tonyyoo8181 <158441876+tonyyoo8181@users.noreply.github.com>
2024-08-18 16:02:07 +0900

    Update loadcell_calib.py

--- ROY/Multi_Camera_Adapter_V2.2_python/loadcell_calib.py
+++ ROY/Multi_Camera_Adapter_V2.2_python/loadcell_calib.py
@@ -7,76 +7,98 @@ from hx711 import HX711  # import the class HX711
 
 try:
     GPIO.setmode(GPIO.BCM)  # set GPIO pin mode to BCM numbering
-    # Create an object hx which represents your real hx711 chip
-    # Required input parameters are only 'dout_pin' and 'pd_sck_pin'
-    hx = HX711(dout_pin=21, pd_sck_pin=20)
-    # Check if we have swap file. If yes that suggest that the program was not
-    # terminated proprly (power failure). We load the latest state.
-    swap_file_name = 'swap_file.swp'
-    # measure tare and save the value as offset for current channel
+    
+    # Create HX711 objects for two load cells
+    hx1 = HX711(dout_pin=21, pd_sck_pin=20)
+    hx2 = HX711(dout_pin=16, pd_sck_pin=12)
+
+    # Check if we have swap files for each HX711. If yes, that suggests that the program was not
+    # terminated properly (power failure). We load the latest state.
+    swap_file_name_1 = 'swap_file_1.swp'
+    swap_file_name_2 = 'swap_file_2.swp'
+
+    # Measure tare and save the value as offset for current channel
     # and gain selected. That means channel A and gain 128
-    err = hx.zero()
-    # check if successful
-    if err:
-        raise ValueError('Tare is unsuccessful.')
+    err1 = hx1.zero()
+    err2 = hx2.zero()
 
-    reading = hx.get_raw_data_mean()
-    if reading:
-        # always check if you get correct value or only False
-            # now the value is close to 0
-        print('Data subtracted by offset but still not converted to units:',
-                  reading)
+    # Check if successful
+    if err1:
+        raise ValueError('Tare for HX711 #1 is unsuccessful.')
+    if err2:
+        raise ValueError('Tare for HX711 #2 is unsuccessful.')
+
+    # Calibration for Load Cell 1
+    reading1 = hx1.get_raw_data_mean()
+    if reading1:
+        print('HX711 #1: Data subtracted by offset but still not converted to units:', reading1)
     else:
-        print('invalid data', reading)
+        print('HX711 #1: Invalid data', reading1)
 
-        # In order to calculate the conversion ratio to some units, in my case I want grams,
-        # you must have known weight.
-    input('Put known weight on the scale and then press Enter')
-    reading = hx.get_data_mean()
-    if reading:
-        print('Mean value from HX711 subtracted by offset:', reading)
-        known_weight_grams = input(
-            'Write how many grams it was and press Enter: ')
+    input('Put known weight on Load Cell 1 and then press Enter')
+    reading1 = hx1.get_data_mean()
+    if reading1:
+        print('HX711 #1: Mean value from HX711 subtracted by offset:', reading1)
+        known_weight_grams_1 = input('Write how many grams it was and press Enter: ')
         try:
-            value = float(known_weight_grams)
-            print(value, 'grams')
+            value1 = float(known_weight_grams_1)
+            print(value1, 'grams')
         except ValueError:
-            print('Expected integer or float and I have got:',
-                  known_weight_grams)
+            print('Expected integer or float and I have got:', known_weight_grams_1)
 
-            # set scale ratio for particular channel and gain which is
-            # used to calculate the conversion to units. Required argument is only
-            # scale ratio. Without arguments 'channel' and 'gain_A' it sets
-            # the ratio for current channel and gain.
-        ratio = reading / value  # calculate the ratio for channel A and gain 128
-        hx.set_scale_ratio(ratio)  # set ratio for current channel
-        print('Ratio is set.')
+        ratio1 = reading1 / value1  # Calculate the ratio for channel A and gain 128
+        hx1.set_scale_ratio(ratio1)  # Set ratio for current channel
+        print('HX711 #1: Ratio is set.')
     else:
-        raise ValueError(
-            'Cannot calculate mean value. Try debug mode. Variable reading:',
-            reading)
+        raise ValueError('Cannot calculate mean value for HX711 #1. Try debug mode. Variable reading:', reading1)
 
-        # This is how you can save the ratio and offset in order to load it later.
-        # If Raspberry Pi unexpectedly powers down, load the settings.
-    print('Saving the HX711 state to swap file on persistant memory')
-    with open(swap_file_name, 'wb') as swap_file:
-        pickle.dump(hx, swap_file)
+    # Calibration for Load Cell 2
+    reading2 = hx2.get_raw_data_mean()
+    if reading2:
+        print('HX711 #2: Data subtracted by offset but still not converted to units:', reading2)
+    else:
+        print('HX711 #2: Invalid data', reading2)
+
+    input('Put known weight on Load Cell 2 and then press Enter')
+    reading2 = hx2.get_data_mean()
+    if reading2:
+        print('HX711 #2: Mean value from HX711 subtracted by offset:', reading2)
+        known_weight_grams_2 = input('Write how many grams it was and press Enter: ')
+        try:
+            value2 = float(known_weight_grams_2)
+            print(value2, 'grams')
+        except ValueError:
+            print('Expected integer or float and I have got:', known_weight_grams_2)
+
+        ratio2 = reading2 / value2  # Calculate the ratio for channel A and gain 128
+        hx2.set_scale_ratio(ratio2)  # Set ratio for current channel
+        print('HX711 #2: Ratio is set.')
+    else:
+        raise ValueError('Cannot calculate mean value for HX711 #2. Try debug mode. Variable reading:', reading2)
+
+    # Save the HX711 states to swap files on persistent memory
+    print('Saving the HX711 #1 state to swap file on persistent memory')
+    with open(swap_file_name_1, 'wb') as swap_file:
+        pickle.dump(hx1, swap_file)
         swap_file.flush()
         os.fsync(swap_file.fileno())
-        # you have to flush, fsynch and close the file all the time.
-        # This will write the file to the drive. It is slow but safe.
+
+    print('Saving the HX711 #2 state to swap file on persistent memory')
+    with open(swap_file_name_2, 'wb') as swap_file:
+        pickle.dump(hx2, swap_file)
+        swap_file.flush()
+        os.fsync(swap_file.fileno())
 
     # Read data several times and return mean value
     # subtracted by offset and converted by scale ratio to
-    # desired units. In my case in grams.
-    print("Now, I will read data in infinite loop. To exit press 'CTRL + C'")
+    # desired units. In this case, grams.
+    print("Now, I will read data from both load cells in an infinite loop. To exit, press 'CTRL + C'")
     input('Press Enter to begin reading')
     while True:
-        print(hx.get_weight_mean(20), 'g')
+        print(f'Load Cell 1: {hx1.get_weight_mean(20)} g, Load Cell 2: {hx2.get_weight_mean(20)} g')
 
 except (KeyboardInterrupt, SystemExit):
     print('Bye :)')
 
 finally:
     GPIO.cleanup()
-
